750007396b9772c305637431de540768f8bd976d
faisal-2d <abdullah71faisal@gmail.com>
2022-07-19 22:36:04 +0600

    search icon changes on fetching

--- src/Components/NoticeBoard/AqeedahResultPage.js
+++ src/Components/NoticeBoard/AqeedahResultPage.js
@@ -11,15 +11,14 @@ const AqeedahResultPage = () => {
   const params = useParams();
   let searchName = '';
   const getStudentList = async (searchName) => {
-    const {data} = await axios.get(`http://localhost:5000/students_${params.batch}/${searchName}`)
+    const {data} = await axios.get(`https://flannel-loonie-61461.herokuapp.com/students_${params.batch}/${searchName}`)
     return data;
   }
 
   const {isLoading, isFetching, data: students, refetch} = useQuery('studentList', () => getStudentList(searchName))
  
  
-
-if(isLoading || isFetching){
+if(isLoading){
   return <Loading></Loading>
 }
 
@@ -28,7 +27,8 @@ const searchByName = e => {
   e.preventDefault();
   const name = e.target.name.value;  
   searchName = name;
-  refetch()
+  refetch();
+  // e.target.reset();
 }
  
 
@@ -40,9 +40,14 @@ const searchByName = e => {
       <div className="form-control my-10">
   <div className="input-group justify-center">
     <input name="name" type="text" placeholder="আপনার নাম লিখে সার্চ করুন" className="input input-bordered w-4/5" />
-    <button type="submit" className="btn btn-square">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    {isFetching 
+    ?
+    <button className="btn btn-primary btn-square loading">
     </button>
+    :
+    <button type="submit" className="btn btn-primary btn-square">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>}
   </div>
 </div>
       </form>
