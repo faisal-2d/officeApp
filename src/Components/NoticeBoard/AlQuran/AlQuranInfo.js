e1ed49569e59be5000994c2a7b352e78eae83457
faisal2d <abdullah71faisal@gmail.com>
2024-10-10 23:49:27 +0300

    courses info updated

--- src/Components/NoticeBoard/AlQuran/AlQuranInfo.js
+++ src/Components/NoticeBoard/AlQuran/AlQuranInfo.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function AlQuranInfo() {
-  return <div>AlQuranInfo</div>;
+  return (
+    <div className="container mx-auto px-5 my-10">
+      {/* Welcome  */}
+      <section>
+        <div className="p-5 rounded bg-gradient-to-tl from-sky-200">
+          <p className="text-3xl">আল কুরআন কোর্সে</p>
+          <p className="text-4xl">স্বাগতম!</p>
+        </div>
+      </section>
+      {/* Course Descripttion  */}
+      <section className="my-10">
+        <p className="text-xl">বিজ্ঞপ্তিঃ</p>
+        <p> শিঘ্রই পেইজটি আপডেট করা হবে, ইনশাআল্লাহ। </p>
+      </section>
+    </div>
+  );
 }
--- src/Components/NoticeBoard/Tafseer/TafseerInfo.js
+++ src/Components/NoticeBoard/Tafseer/TafseerInfo.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const TafseerInfo = () => {
-    return (
-        <div className='container mx-auto px-5 my-10'>
-            {/* Welcome  */}
-           <section>
-                <div className='p-5 rounded bg-gradient-to-tl from-sky-200'>
-                    <p className='text-3xl'>সংক্ষিপ্ত তাফসীর কোর্সে</p>
-                    <p className='text-4xl'>স্বাগতম!</p>
-                </div>
-           </section>
-           {/* Course Descripttion  */}
-           <section className='my-10'>
-                <p className='text-xl'>বিজ্ঞপ্তিঃ</p>
-                <p> এই কোর্সে নতুন ভর্তি নেওয়া হচ্ছে না। </p>
-                <p>কোর্সটি পরিচালনা করতেন শায়খ আব্দুল্লাহ আল কাফী (রহিমাহুল্লাহ)। তিনি ১৬ আগস্ট ২০২৩ তারিখে ইন্তেকাল করেন।</p>
-                <p>আল্লাহ সুবহানাহু ওয়া তায়ালা শায়খকে জান্নাতুল ফিরদাউস দান করুন। আমীন।</p>
-                <p className='pt-3'>পরবর্তীতে এই কোর্সটি আবার শুরু হলে আমাদের ফেসবুক পেইজে বিজ্ঞপ্তি দেওয়া হবে।</p>
-           </section>
+  return (
+    <div className="container mx-auto px-5 my-10">
+      {/* Welcome  */}
+      <section>
+        <div className="p-5 rounded bg-gradient-to-tl from-sky-200">
+          <p className="text-3xl">সংক্ষিপ্ত তাফসীর কোর্সে</p>
+          <p className="text-4xl">স্বাগতম!</p>
         </div>
-    );
+      </section>
+      {/* Course Descripttion  */}
+      <section className="my-10">
+        <p className="text-xl">বিজ্ঞপ্তিঃ</p>
+        <p> শিঘ্রই পেইজটি আপডেট করা হবে, ইনশাআল্লাহ। </p>
+      </section>
+    </div>
+  );
 };
 
 export default TafseerInfo;
